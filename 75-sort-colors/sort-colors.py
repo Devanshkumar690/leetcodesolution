3cdb39a202752e4c9ec4bf99886706a64ae669af
Devansh kumar <241978235+Devanshkumar690@users.noreply.github.com>
2026-06-28 01:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.46%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,19 +1,19 @@
-class Solution(object):
-    def sortColors(self, nums):
-        z=0
-        o=0
-        t=0
-        lst=[]
-        for i in range(len(nums)):
-            if nums[i]==0 :
-                z=z+1
-            elif nums[i]==1 :
-                o=o+1
-            elif nums[i]==2 :
-                t=t+1       
-        lst += [0] * z
-        lst += [1] * o
-        lst += [2] * t
-        for i in range(len(nums)):
-            nums[i] = lst[i]
-        return nums
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low=0
+        mid=0
+        high = len(nums) - 1
+        while mid<=high:
+            if(nums[mid]==0):
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low+=1
+                mid+=1
+            elif(nums[mid]==1):
+                mid+=1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high-=1
+        return nums        
+
+
+        
