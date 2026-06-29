3cd9f012011fca0f760d4542f61c1c327ffdaa08
Devansh kumar <241978235+Devanshkumar690@users.noreply.github.com>
2026-06-30 01:30:30 +0530

    Time: 1 ms (99.94%) | Memory: 69.3 MB (10.19%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -15,11 +15,6 @@ class Solution {
                 }
             high++;
         }
-        if(res == Integer.MAX_VALUE){
-            return 0;
-        }
-        else{
-            return res;
-        }
+       return res == Integer.MAX_VALUE ? 0 : res;
     }
 }
