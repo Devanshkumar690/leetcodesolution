acc3433495584e27f70227729ff2d80ed078772e
Devansh kumar <241978235+Devanshkumar690@users.noreply.github.com>
2026-07-04 22:39:27 +0530

    Time: 27 ms (24.89%) | Memory: 47.2 MB (11.77%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,17 +3,15 @@ class Solution {
         HashMap<Character,Integer> map =new HashMap<>();
         int low =0;
         int res =0;
+        int maxFreq =0;
         for(int high=0;high<s.length();high++){
             char ch = s.charAt(high);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
 
+          
+            maxFreq = Math.max(maxFreq, map.get(ch));    
             int len =high-low+1;
-            int maxFreq = 0;
-            for (int freq : map.values()) {
-                    maxFreq = Math.max(maxFreq, freq);
-                }       
-            int need = len - maxFreq;
-            while(need>k){
+            while(len-maxFreq>k){
                 char left =s.charAt(low);
                 if(map.get(left)==1){
                     map.remove(left);
@@ -22,15 +20,10 @@ class Solution {
                     map.put(left, map.get(left) - 1);
                 }
                 low++;
-                maxFreq =0;
-                for (int freq : map.values()) {
-                    maxFreq = Math.max(maxFreq, freq);
-                }
-                len =high-low+1; 
-                need = len - maxFreq;
                 
+                len =high-low+1; 
              }
-            len = high-low+1;
+           
             res = Math.max(res,len);
         }
         
