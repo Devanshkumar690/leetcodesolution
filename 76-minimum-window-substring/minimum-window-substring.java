653b9b61b9cec864bb126ad623e3659ca035c436
Devansh kumar <241978235+Devanshkumar690@users.noreply.github.com>
2026-07-11 20:54:50 +0530

    Time: 41 ms (19.78%) | Memory: 45.8 MB (65.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,55 +1,40 @@
 class Solution {
+    public boolean valid(int[] freqs, int[] freqt){
+            for(int i=0;i<128;i++){
+                if(freqs[i]<freqt[i]){
+                    return false;
+                }
+            }
+            return true;
+    }
     public String minWindow(String s, String t) {
 
         if (s.length() < t.length()) return "";
 
-        int[] freq = new int[128];
-
-       
-        for (char ch : t.toCharArray()) {
-            freq[ch]++;
+        int[] freqt = new int[128];
+        for(int i=0;i<t.length();i++){
+            freqt[t.charAt(i)]++;
         }
+        int low=0;
+        int res= Integer.MAX_VALUE;
+        int[] freqs = new int[128];
+        int start=0;
+        for(int high =0;high<s.length();high++){
+            freqs[s.charAt(high)]++;
+            while(valid(freqs,freqt)){
+                int len =high-low+1;
 
-        int low = 0;
-        int start = 0;
-        int minLen = Integer.MAX_VALUE;
-
-        int required = t.length();
-
-        for (int high = 0; high < s.length(); high++) {
-
-            char right = s.charAt(high);
-
-          
-            if (freq[right] > 0) {
-                required--;
-            }
-
-            freq[right]--;
-
-    
-            while (required == 0) {
-
-                if (high - low + 1 < minLen) {
-                    minLen = high - low + 1;
+                if(res>len){
+                    res=len;
                     start = low;
                 }
-
-                char left = s.charAt(low);
-
-                freq[left]++;
-
-       
-                if (freq[left] > 0) {
-                    required++;
-                }
-
+                freqs[s.charAt(low)]--;
                 low++;
             }
         }
-
-        return minLen == Integer.MAX_VALUE
-                ? ""
-                : s.substring(start, start + minLen);
+        return (res == Integer.MAX_VALUE)
+                 ? ""
+                : s.substring(start, start + res);
+        
     }
 }
